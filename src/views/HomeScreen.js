d443ede913bf6e20316e89bc9e9db58caf7832ec
Raymond Yang <raymond.yang@centrapay.com>
2022-02-27 13:15:39 +1300

    Clean up HomeScreen styling

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -7,10 +7,10 @@ import typography from '../styles/typography';
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={[styles.container, { backgroundColor: colors.background }]}>
+    <ScrollView>
+      <View style={styles.container}>
         <View style={styles.titleContainer}>
-          <Text style={[styles.title, { color: colors.primary }]}>Tracker</Text>
+          <Text style={styles.title}>Tracker</Text>
         </View>
         <Navbar />
         <CardList onCardPress={selectedCoin => navigation.navigate('Details', { selectedCoin })} />
@@ -22,24 +22,17 @@ const HomeScreen = ({ navigation }) => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   container: {
     alignItems: 'center',
-    width: '100%',
-    justifyContent: 'center',
-    paddingTop: '8%',
+    paddingVertical: '8%',
+    backgroundColor: colors.background,
   },
   titleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
     marginBottom: '6%',
-    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     ...typography.title,
+    color: colors.primary,
   },
 });

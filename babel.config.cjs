d1c6e81546a9f041720c8f44948c6dc5bde9f8db
Raymond Yang <raymond.yang@centrapay.com>
2022-02-27 17:54:48 +1300

    Fix babel loose property

--- babel.config.cjs
+++ babel.config.cjs
@@ -10,7 +10,8 @@ module.exports = {
     ]
   ],
   plugins: [
-    '@babel/plugin-proposal-private-property-in-object',
-    { 'loose': true }
+    ['@babel/plugin-proposal-private-property-in-object', {
+      'loose': true
+    }]
   ]
 };

768351e8bb6a079bd9a578e1b5aef49604fdacff
Raymond Yang <raymond.yang@centrapay.com>
2022-05-03 22:05:13 +1200

    Fix startup error on Android

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import 'intl';
+import { Platform } from 'react-native';
+
+if (Platform.OS === 'android') {
+  // See https://github.com/expo/expo/issues/6536 for this issue.
+  if (typeof Intl.__disableRegExpRestore === 'function') {
+    Intl.__disableRegExpRestore();
+  }
+}
+import 'intl/locale-data/jsonp/en';
+
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
